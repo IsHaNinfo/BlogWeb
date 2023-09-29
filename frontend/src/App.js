bb9a777d42413066286f8c686093265d1d219a40
ishaninfo56@gmail.com <120440668+IsHaNinfo@users.noreply.github.com>
2023-09-29 13:44:46 +0530

    add page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <header>
+          <a href="" className="logo">
+            MyBlog
+          </a>
+          <nav>
+            <a href="">Login</a>
+            <a href="">Register</a>
+          </nav>
+        </header>
+        <div className="post">
+          <div className="image">
+            <img src="https://cdn-images-1.medium.com/max/1000/1*wUgo26aGsy1HMerugYrYsg.jpeg"></img>
+          </div>{" "}
+          <div className="texts">
+            <h2>The DevOps Lifecycle</h2>
+            <p>
+              DevOps is a collection of cultural philosophies, practices, and
+              tools that increase an organization's ability to deliver
+              applications and services at high velocity.
+            </p>
+          </div>
+        </div>
+        <div className="post">
+          <div className="image">
+            <img src="https://cdn-images-1.medium.com/max/1000/1*wUgo26aGsy1HMerugYrYsg.jpeg"></img>
+          </div>
+          <div className="texts">
+            <h2>The DevOps Lifecycle</h2>
+            <p>
+              DevOps is a collection of cultural philosophies, practices, and
+              tools that increase an organization's ability to deliver
+              applications and services at high velocity.
+            </p>
+          </div>
+        </div>
+        <div className="post">
+          <div className="image">
+            <img src="https://cdn-images-1.medium.com/max/1000/1*wUgo26aGsy1HMerugYrYsg.jpeg"></img>
+          </div>{" "}
+          <div className="texts">
+            <h2>The DevOps Lifecycle</h2>
+            <p>
+              DevOps is a collection of cultural philosophies, practices, and
+              tools that increase an organization's ability to deliver
+              applications and services at high velocity.
+            </p>
+          </div>
+        </div>
+        <div className="post">
+          <div className="image">
+            <img src="https://cdn-images-1.medium.com/max/1000/1*wUgo26aGsy1HMerugYrYsg.jpeg"></img>
+          </div>{" "}
+          <div className="texts">
+            <h2>The DevOps Lifecycle</h2>
+            <p>
+              DevOps is a collection of cultural philosophies, practices, and
+              tools that increase an organization's ability to deliver
+              applications and services at high velocity.
+            </p>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
